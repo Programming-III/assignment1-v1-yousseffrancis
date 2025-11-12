9a0ff71a46cc0a38e2a28e1a4d8e267a82a65843
yousseffrancis <yousseffransisco@gmail.com>
2025-11-12 15:22:57 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,17 @@
-#ifndef REPTILE_H
-#define REPTILE_H
 
-#include "Animal.h"
+#ifndef REPTILE_H_INCLUDED
+#define REPTILE_H_INCLUDED
+#include<iostream>
+#include<string>
+using namespace std;
+#include"Animal.h"
+class Reptile : public Animal{
+private:
+   bool isVenomous;
+public:
+    Reptile();
+    Reptile(bool isVenomous);
+    ~Reptile();
+};
 
-//define reptile class here
-
-
-
-
-
-
-
-
-
-#endif
+#endif // REPTILE_H_INCLUDED
