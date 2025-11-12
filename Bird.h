21952db1d50425862d066e0c090d390b3b51f689
yousseffrancis <yousseffransisco@gmail.com>
2025-11-12 15:19:29 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,18 @@
-#ifndef BIRD_H
-#define BIRD_H
 
-#include "Animal.h"
+#ifndef BIRD_H_INCLUDED
+#define BIRD_H_INCLUDED
+#include<iostream>
+#include<string>
+using namespace std;
+#include"Animal.h"
+class Bird : public Mammal{
+private:
+    float wingSpan;
+public:
+    Bird();
+    Bird(float wingSpan);
+    ~Bird();
+};
 
-//define class bird here 
 
-
-
-
-
-
-
-#endif
+#endif // BIRD_H_INCLUDED
